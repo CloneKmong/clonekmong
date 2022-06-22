4c39b8a0ba66a3e25e8ec0b36c9858b9202b6526
sol <osunpine@gmail.com>
2022-06-22 13:48:04 +0900

    create page axios 추가

--- src/redux/modules/PostSlice.js
+++ src/redux/modules/PostSlice.js
@@ -6,16 +6,18 @@ const SERVER_URL = "http://13.209.22.194";
 // Main View 리스트 가져오기
 export const addProjectList = createAsyncThunk(
   "ADD/addProjectList",
-  async (formData) => {
-    return await axios
-      .post(`${SERVER_URL}/projects/project`, formData, {
+  async (newList) => {
+    const response = await axios.post(
+      `${SERVER_URL}/projects/project`,
+      newList,
+      {
         headers: {
-          "Content-Type": "multipart/form-data",
+          //   "Content-Type": "multipart/form-data",
           Authorization: localStorage.getItem("access_token"),
         },
-      })
-      .then((res) => console.log("res"))
-      .catch((e) => console.log("error"));
+      }
+    );
+    return response.data;
   }
 );
 
